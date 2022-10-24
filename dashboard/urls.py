bdc62105dc744a22ecf342be81ea7ccabdac877f
amilmshaji <msamilmanadiyil2560@gmail.com>
2022-10-24 15:54:47 +0530

    change password

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('myprofile/', views.dashboard, name='myprofile'),
-    path('editprofile/', views.editprofile, name='editprofile'),
+    path('editprofile/', views.editprofile, name='edit_profile'),
+    path('changePassword/', views.changePassword, name='changePassword'),
 
     path('myproducts/', views.myproducts, name='myproducts'),
 

d333f12cae5e808cfa73ead43ae6149b403aee3e
amilmshaji <msamilmanadiyil2560@gmail.com>
2022-11-14 21:26:46 +0530

    updated

--- products/admin.py
+++ products/admin.py
@@ -82,13 +82,13 @@ class CarAdmin(admin.ModelAdmin):
     list_editable = ['is_available']
 
     # This will help you to disbale add functionality
-    # def has_add_permission(self, request):
-    #     return False
-    #
-    #     # This will help you to disable delete functionaliyt
-    #
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
+    def has_add_permission(self, request):
+        return False
+
+        # This will help you to disable delete functionaliyt
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
     def thumbnail_preview1(self, obj):

49105cdf8ac2b1ed6a20e7e921c3130f2a2f8a9e
diyajacobe <diyamoljacob2023a@mca.ajce.in>
2023-02-23 16:05:52 +0530

    deleted some error codes

--- anyrent_pjct/urls.py
+++ anyrent_pjct/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.urls import path
 from django.urls.conf import include
-from django.conf import settings
+from django.conf import setting
 
 admin.site.unregister(Group)
 

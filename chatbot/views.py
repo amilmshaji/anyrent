4cb380b7c4d09f2c66c21bcd27c708663f109ff2
diyajacobe <diyamoljacob2023a@mca.ajce.in>
2023-05-10 14:47:19 +0530

    navigation completed

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,70 +1,11 @@
 import webbrowser
-
+import requests
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.template.defaultfilters import safe
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
-import torch
-
-#
-# # Define a function to handle user input and generate a response
-# def get_response(user_input):
-# payload = {
-#             "inputs": {
-#                 "question": question,
-#                 "context": context
-#             }
-#         }
-#         response = requests.post(API_URL, headers=headers, json=payload)
-#         answer = response.json()['answer']
-
-
-#         tokenizer = AutoTokenizer.from_pretrained('squirro/albert-base-v2-squad_v2')
-#         model = AutoModelForQuestionAnswering.from_pretrained('squirro/albert-base-v2-squad_v2')
-#         inputs = tokenizer.encode_plus(user_input, context, add_special_tokens=True, return_tensors="pt")
-#         input_ids = inputs["input_ids"].tolist()[0]
-#         outputs = model(**inputs)
-#         answer_start_scores = outputs.start_logits
-#         answer_end_scores = outputs.end_logits
-#         answer_start = torch.argmax(answer_start_scores)
-#         answer_end = torch.argmax(answer_end_scores) + 1
-#         response = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-#     return response
-#
-#
-# # Define a function to handle the chatbot
-# import speech_recognition as sr
-# #
-# #
-# def chatbott(request):
-#     if request.method == 'POST':
-#         print("hello")
-#         question = request.POST['question']
-#         answer = get_response(question)
-#         if not user_input:
-#             # If user input is empty, try speech recognition
-#             r = sr.Recognizer()
-#             with sr.Microphone() as source:
-#                 audio = r.listen(source)
-#             try:
-#                 user_input = r.recognize_google(audio)
-#             except sr.UnknownValueError:
-#                 pass
-#             except sr.RequestError as e:
-#                 return render(request, 'chatbot.html', {'response': str(e)})
-#         print(answer)
-#         response_data = {'question': question,'answer': answer}
-#         return JsonResponse(response_data)
-
-
-    # return render(request, 'chatbot.html', {'response': response})
-#     else:
-#         return render(request, 'guidance.html')
-# from django.shortcuts import render
 from products.models import House_Product
-
-tokenizer = AutoTokenizer.from_pretrained('squirro/albert-base-v2-squad_v2')
-model = AutoModelForQuestionAnswering.from_pretrained('squirro/albert-base-v2-squad_v2')
+API_URL = "https://api-inference.huggingface.co/models/squirro/albert-base-v2-squad_v2"
+headers = {"Authorization": "Bearer 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"}
 
 def answer_question(question, context):
     pages = ["home","map","category", "shop", "login","logout", "register","myprofile"]
@@ -82,14 +23,14 @@ def answer_question(question, context):
         answer= "Here are some options:\nhome,\nmap,\ncategory\n,shop\n,login,\nregister,\nlogout"
     else:
 
-        inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
-        input_ids = inputs["input_ids"].tolist()[0]
-        outputs = model(**inputs)
-        answer_start_scores = outputs.start_logits
-        answer_end_scores = outputs.end_logits
-        answer_start = torch.argmax(answer_start_scores)
-        answer_end = torch.argmax(answer_end_scores) + 1
-        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+        payload = {
+            "inputs": {
+                "question": question,
+                "context": context
+            }
+        }
+        response = requests.post(API_URL, headers=headers, json=payload)
+        answer = response.json()['answer']
     print(answer)
     if answer == "[CLS]" or answer =='':
         answer = "sorry i cannot find the result. Can you specify with the context"
